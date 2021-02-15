bef0cff8ed6690ad4991c1f0027c7f4a9863e9f5
FacuBargut <facubargut@gmail.com>
2021-02-15 20:36:25 -0300

    Proyecto base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'luca-frontennd-app'`, () => {
+  it(`should have as title 'luca-frontend-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('luca-frontennd-app');
+    expect(app.title).toEqual('luca-frontend-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('luca-frontennd-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('luca-frontend-app app is running!');
   });
 });
